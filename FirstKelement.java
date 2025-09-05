3aca1841af1d73baec917dc93e73f132dff85b10
AniruddhaKalubarme <aniruddhakalubarme@gmail.com>
2025-09-05 17:18:14 +0530

    Added the FirstKelement

---
+++ FirstKelement.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class FirstKelement
+{
+    static int FindElement(int arr[], int k, int n)
+    {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int i = 0; i<n; i++)
+        {
+            if(mp.containsKey(arr[i]))
+            {
+                int temp = mp.get(arr[i]);
+                mp.put(arr[i], ++temp);
+            }
+            else
+            {
+                mp.put(arr[i], 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> key : mp.entrySet())
+        {
+            if(key.getValue() == k)
+                return key.getKey();
+        }
+
+        return -1;
+    }
+
+    public static void main(String []arg)
+    {
+        int k = 3;
+        // int arr[] = {2, 3, 4, 2, 2, 5, 5};
+        // int arr[] = {1, 1, 1, 1};
+        int arr[] = {6, 6, 6, 6, 7, 7, 8, 8, 8};
+
+        System.out.println(FindElement(arr, k, arr.length));
+    }
+}
