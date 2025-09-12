1331f7f261ed8ab3aaf3ec98603ef692b0fb25bb
AniruddhaKalubarme <aniruddhakalubarme@gmail.com>
2025-09-12 20:00:37 +0530

    Added Fibonacchi using DP

---
+++ FiboDP.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class FiboDP
+{
+    static int Fibo(int n, int []arr)
+    {
+        if(n == 0)
+            return 0;
+        
+        if(n == 1)
+            return 1;
+
+        if(arr[n] != -1)
+            return arr[n];
+
+
+        arr[n] = (Fibo(n-1, arr)+Fibo(n-2, arr));
+
+
+        return arr[n];
+    }
+
+    public static void main(String arg[])
+    {
+        int n = 8;
+        int arr[] = new int[n+1];
+
+        for(int i = 0; i<=n; i++)
+            arr[i] = -1;
+
+        System.out.println(Fibo(n, arr));
+    }
+}
